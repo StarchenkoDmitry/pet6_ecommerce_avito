cdc9c1c81007e4149faeb6987a354042fd2bdb1d
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-01-28 15:12:47 +0300

    refactor

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -12,14 +12,12 @@ interface Props {
 function Chat({chat,messages}: Props) {
 
     return (
-        <div className="flex flex-col flex-1    h-full _max-h-[300px]">
-            {/* <div className="flex flex-col flex-1      _h-full     overflow-y-auto overflow-hidden"> */}
-                <div className="my-2 p-2 flex-1 basis-0   overflow-hidden overflow-y-auto bg-gray-100 rounded-lg">
-                {
-                    messages.map(e=>(<MessageBox key={e.id} message={e}/>))
-                }
-                </div>
-            {/* </div> */}
+        <div className="flex flex-col flex-1 h-full">
+            <div className="my-2 p-2 flex-1 basis-0 overflow-hidden overflow-y-auto bg-gray-100 rounded-lg">
+            {
+                messages.map(e=>(<MessageBox key={e.id} message={e}/>))
+            }
+            </div>
             <div>
                 <input 
                     className="border-2 border-gray-600 p-1 "
