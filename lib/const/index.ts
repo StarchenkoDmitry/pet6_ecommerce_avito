2d068a90ab48a61e3f45eb3ccaca07f2ce2878c5
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-01 22:02:53 +0300

    change const file

--- lib/const/index.ts
+++ lib/const/index.ts
@@ -26,15 +26,22 @@ export const MAX_ASPECT_RATION_PICTURE = 3;//max aspect ratio pricture
 
 export const MAX_PIXELS_IN_PICTURE = MAX_WIDTH_PICTURE * MAX_HEIGHT_PICTURE;
 
+
+export const PICTURE_SCALE_HEIGHT_MAX = 801;
 export const PICTURE_SCALE_HEIGHT_0 = 800;
 export const PICTURE_SCALE_HEIGHT_1 = 400;
 export const PICTURE_SCALE_HEIGHT_2 = 200;
 export const PICTURE_SCALE_HEIGHT_3 = 64;
 
 
-export const MAX_ASPECT_RATION_AVATAR = 2;//max aspect ratio pricture
 
-// const MAX_WIDTH_AVATAR_IMAGE_LEVEL_0 = 64;
-// const MAX_WIDTH_AVATAR_IMAGE_LEVEL_1 = 256;
-export const AVATAR_SCALE_HEIGHT_0 = 200;
-export const AVATAR_SCALE_HEIGHT_1 = 64;
+export const MAX_WIDTH_AVATAR = 4*1024;
+export const MAX_HEIGHT_AVATAR = 4*1024;
+
+export const MAX_ASPECT_RATION_AVATAR = 2;//max aspect ratio pricture
+export const MAX_PIXELS_AVATAR = MAX_WIDTH_AVATAR * MAX_HEIGHT_AVATAR;//8MiB
+export const MAX_FILE_SIZE_AVATAR = 1024*1024*8;//8MiB
+
+export const AVATAR_SCALE_WIDTH_MAX = 257;
+export const AVATAR_SCALE_WIDTH_0 = 256;
+export const AVATAR_SCALE_WIDTH_1 = 64;
