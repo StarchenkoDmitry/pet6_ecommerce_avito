99bf2317c7e537519a59be9f6c57e1977b0da26a
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-01 21:57:51 +0300

    add Avatar

---
+++ components/ui/Avatar.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import clsx from "clsx";
+import { 
+    AVATAR_SCALE_WIDTH_0,
+    AVATAR_SCALE_WIDTH_1,
+    AVATAR_SCALE_WIDTH_MAX,
+} from "@/lib/const";
+
+
+interface Props {
+    id:string | null;
+    className?:string;
+
+    sizes?:string;
+    width?:number;
+    height?:number;
+
+    alt?:string;
+}
+
+function Avatar({id,className,height,width,sizes,alt}: Props) {
+    if(id){
+        const imageUrl = `/api/avatar/${id}`;
+        const srcSeet = createAvatarSrcSet(id);
+        return (
+            <img 
+                className={clsx("border-2",className)}
+                src={imageUrl}
+                srcSet={srcSeet}
+                width={width}
+                height={height}
+                sizes={sizes? sizes : "250px"}
+                alt={alt ? alt : "avatar"}
+            />
+        )
+    }else{
+        return(
+            <Image 
+                className={clsx("border-2",className)}
+                src={"/images/user.png"}
+                width={width ? width : 256}
+                height={height ? height : 256}
+                quality={90}
+                sizes={sizes? sizes : "250px"}
+                alt={alt ? alt : "without avatar"}
+            />
+        )
+    }
+}
+
+export default Avatar;
+
+
+function createAvatarSrcSet(id:string){
+    const bs = `/api/avatar/${id}`;
+    const sets = 
+    bs+`?w=${AVATAR_SCALE_WIDTH_1} ${AVATAR_SCALE_WIDTH_1}w, `+
+    bs+`?w=${AVATAR_SCALE_WIDTH_0} ${AVATAR_SCALE_WIDTH_0}w, `+
+    bs+` ${AVATAR_SCALE_WIDTH_MAX}w`;
+    return sets;
+}
