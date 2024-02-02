354680b1bb785418d7d3d4b94a83f3203aa98ace
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-02 20:05:54 +0300

    change const file

--- lib/const/index.ts
+++ lib/const/index.ts
@@ -16,30 +16,32 @@ export const DEFAULT_ITEM_PRICE = 0;
 
 
 
-export const MAX_COUNT_PICTURES = 12;//maximum number of pictures
+export const MAX_COUNT_PICTURES = 12;//maximum count of pictures
 // export const MIN_COUNT_PICTURES = 0;
 
+export const MAX_ASPECT_RATION_PICTURE = 3;
 export const MAX_WIDTH_PICTURE = 4*1024;
 export const MAX_HEIGHT_PICTURE = 4*1024;
-
-export const MAX_ASPECT_RATION_PICTURE = 3;//max aspect ratio pricture
+// export const MIN_WIDTH_PICTURE = 16;
+// export const MIN_HEIGHT_PICTURE = 16;
 
 export const MAX_PIXELS_IN_PICTURE = MAX_WIDTH_PICTURE * MAX_HEIGHT_PICTURE;
+export const MAX_FILE_SIZE_PICTURE = 1024*1024*8;//8MiB
+
+export const PICTURE_SCALE_WIDTH_MAX = 801;
+export const PICTURE_SCALE_WIDTH_0 = 800;
+export const PICTURE_SCALE_WIDTH_1 = 400;
+export const PICTURE_SCALE_WIDTH_2 = 250;
+export const PICTURE_SCALE_WIDTH_3 = 64;
 
 
-export const PICTURE_SCALE_HEIGHT_MAX = 801;
-export const PICTURE_SCALE_HEIGHT_0 = 800;
-export const PICTURE_SCALE_HEIGHT_1 = 400;
-export const PICTURE_SCALE_HEIGHT_2 = 200;
-export const PICTURE_SCALE_HEIGHT_3 = 64;
 
 
-
+export const MAX_ASPECT_RATION_AVATAR = 2;
 export const MAX_WIDTH_AVATAR = 4*1024;
 export const MAX_HEIGHT_AVATAR = 4*1024;
 
-export const MAX_ASPECT_RATION_AVATAR = 2;//max aspect ratio pricture
-export const MAX_PIXELS_AVATAR = MAX_WIDTH_AVATAR * MAX_HEIGHT_AVATAR;//8MiB
+export const MAX_PIXELS_AVATAR = MAX_WIDTH_AVATAR * MAX_HEIGHT_AVATAR;
 export const MAX_FILE_SIZE_AVATAR = 1024*1024*8;//8MiB
 
 export const AVATAR_SCALE_WIDTH_MAX = 257;
