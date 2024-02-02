3cd27e6d9b6b1d42357791cdbe46810d797ffbbc
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-02 22:27:44 +0300

    change FavoriteItem

--- components/item/FavoriteItem.tsx
+++ components/item/FavoriteItem.tsx
@@ -1,21 +1,22 @@
-/* eslint-disable @next/next/no-img-element */
 'use client'
 
+import { useState } from 'react';
+
 import { HeartIcon as HeartIconSolid } from '@heroicons/react/24/solid';
-import Image from 'next/image';
 import { HeartIcon as HeartIconOutLine } from '@heroicons/react/24/outline';
+
 import { Item } from '@prisma/client';
-import { useRef, useState } from 'react';
 import { changeFavorite } from '@/lib/actions/favorite';
+import ItemImage from '../ui/ItemImage';
+
 
 export interface Props {
     item: Item;
 }
 
 function FavoriteItem({item}: Props) {
-    const { id, imageId, lable, price } = item;
+    const { id, mainImageId, lable, price } = item;
 
-    const imageUrl = imageId ? `/api/image/${imageId}` : "/img/1.jpg";
     const itemUrl = `/item/${id}`;
 
     const [isFavorite,setIsFavorite] = useState(true);
@@ -34,16 +35,14 @@ function FavoriteItem({item}: Props) {
     return (
         <div className="m-2 p-2 w-64 bg-white rounded-lg">
             <a href={itemUrl}>
-                <img
+                <ItemImage
                     className="w-[250px] h-[200px] object-cover  rounded"
-                    src={imageUrl}
-                    alt="item"
-                    width={50}
-                    height={250}
+                    id={mainImageId}
+                    sizes="250px"
                 />
             </a>
-            <div className='flex _flex-wrap _flex-none justify-between'>
-                <a href={"/"} className='p-1 line-clamp-2 break-words text-sm text-blue-500'>
+            <div className='flex justify-between'>
+                <a href={itemUrl} className='p-1 line-clamp-2 break-words text-sm text-blue-500'>
                     {lable}
                 </a>
                 <button className='flex-none flex justify-center items-center m-1 w-6 h-6' onClick={handelChange}>
@@ -54,7 +53,7 @@ function FavoriteItem({item}: Props) {
                 }
                 </button>
             </div>
-            <span className='px-1 block text-sm font-medium'>{price} ₽</span>
+            <span className='px-1 block text-sm font-medium'>$4 {price}</span>
         </div>
     )
 }
