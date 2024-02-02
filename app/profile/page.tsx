48fb2e328019c686221e68d2af97c5e1f9473fc5
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-02 03:11:26 +0300

    refactor Profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,14 @@
 import Profile from '@/components/profile/Profile';
 import Breadcrumbs from '@/components/ui/Breadcrumbs';
-import { auth } from '@/config/authConfig';
 import db from '@/lib/db';
 
 
 export default async function Home() {
-  console.log("Render Profile page");
-
   const user = await db.user.currentUser();
 
   if(user){
     return (
-      <>
-        <h2>Profile page</h2>
+      <div className='p-2'>
         <Breadcrumbs breadcrumbs={[
           {
             href:"/profile",
@@ -26,7 +22,7 @@ export default async function Home() {
         <div className="p-2 bg-gray-50 rounded-lg">
           <Profile user={user}/>
         </div>
-      </>
+      </div>
     )
   }
   return (
@@ -35,37 +31,3 @@ export default async function Home() {
     </div>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-// export default async function Home() {
-//   console.log("Render Main page");
-
-//   const session = await auth();
-
-
-//   if(session){
-//     const user = await db.user.findFirst({ where:{ id:session.user.userId }});
-//     if(user){
-//       return (
-//         <div className="p-2 bg-gray-50 rounded-lg">
-//           <span>Profile page</span>
-//           <Profile user={user}/>
-//         </div>
-//       )
-//     }
-//   }
-//   return (
-//     <div className="p-2 bg-gray-50 rounded-lg">
-//       <span className='mx-1 text-xl'>Зарегистрируйтесь</span>
-//     </div>
-//   )
-// }
