1f7f0e2250cd8889a80f8fea21b7588c45e0d231
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-02 03:18:57 +0300

    change User page

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,35 +1,39 @@
-/* eslint-disable @next/next/no-img-element */
 import db from "@/lib/db";
+import Avatar from "@/components/ui/Avatar";
+
 
 export default async function Home({ params }: { params: { id: string } }) {
-  const { id } = params;
-  console.log("Render User page");
+    const { id } = params;
 
-  const user = await db.user.findFirst({
-    where:{ id:id },
-  });
+    const user = await db.user.findFirst({
+        where: { id: id },
+        select: {
+            id: true,
+            name: true,
+            surname: true,
+            imageId: true,
+        },
+    });
 
-  if(!user){
+    if (!user) {
+        return (
+            <div className="p-2 flex">
+                <h2 className="p-1">User with id {id} is not exist</h2>
+            </div>
+        );
+    }
+
     return (
-      <div className="">
-        <h2 className="p-1">User with id {id} is not exist</h2>
-      </div>
+        <div className="p-2 flex">
+            <Avatar
+                className="w-[128px] h-[128px] object-cover rounded-full"
+                id={user.imageId}
+                sizes="128px"
+            />
+            <div className="p-2 flex flex-col">
+                <span>Name: {user.name}</span>
+                <span>Surname: {user.surname}</span>
+            </div>
+        </div>
     );
-  }
-
-  const avatarUrl = user.imageId ? `/api/avatar/${user.imageId}` : "/img/1.jpg";
-
-  return (
-    <div className="">
-      <h2 className="p-1">User id {id}</h2>
-      <span>Name: {user.name}</span>
-      <span>Surname: {user.surname}</span>
-      <span>Surname: {user.imageId}</span>
-      <img
-        className="w-32 h-32 object-cover rounded-full"
-        src={avatarUrl}
-        alt="avatar"
-      />
-    </div>
-  );
 }
