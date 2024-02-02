e943bde6487a730e44c3ef136a1e6d5af4bfd731
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-02 20:17:34 +0300

    refactor api image

--- app/api/image/[id]/route.ts
+++ app/api/image/[id]/route.ts
@@ -1,5 +1,11 @@
 import db from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import { 
+    PICTURE_SCALE_WIDTH_0,
+    PICTURE_SCALE_WIDTH_1,
+    PICTURE_SCALE_WIDTH_2,
+    PICTURE_SCALE_WIDTH_3 
+} from "@/lib/const";
 
 
 export async function GET(
@@ -7,22 +13,65 @@ export async function GET(
     { params }: { params: { id: string } }
 ) {
     try {
+        const searchParams = req.nextUrl.searchParams
+        const widthStr = searchParams.get('w')
         const id = params.id;
 
-        const res = await db.itemImage.findFirst({
-            where:{
-                id:id
-            },
-            select:{
-                buffer:true
+        console.log("ID: ",id);
+        console.log("width: ",widthStr);
+
+        if(!widthStr){
+            const image = await db.itemImage.findFirst({
+                where:{ id:id },select:{ buffer:true }
+            });
+            
+            if(image){
+                return new NextResponse(image.buffer);
             }
-        });
-    
-        if(res){
-            return new NextResponse(res.buffer);
         }else{
-            return new NextResponse(null,{status:404});
+            const width = parseInt(widthStr);
+            if(!Number.isFinite(width)){
+                return new NextResponse(null,{status:400});
+            }
+
+            if(width <= PICTURE_SCALE_WIDTH_3){
+                const image = await db.itemImage.findFirst({
+                    where:{ id:id },select:{ buffer3:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer3);
+                }
+            }else if(width <= PICTURE_SCALE_WIDTH_2){
+                const image = await db.itemImage.findFirst({
+                    where:{ id:id },select:{ buffer2:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer2);
+                }
+            }else if(width <= PICTURE_SCALE_WIDTH_1){
+                const image = await db.itemImage.findFirst({
+                    where:{ id:id },select:{ buffer1:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer1);
+                }
+            }else if(width <= PICTURE_SCALE_WIDTH_0){
+                const image = await db.itemImage.findFirst({
+                    where:{ id:id },select:{ buffer0:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer0);
+                }
+            }else{
+                const image = await db.itemImage.findFirst({
+                    where:{ id:id },select:{ buffer:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer);
+                }
+            }
         }
+        return new NextResponse(null,{status:404});
     } catch (error) {
         console.log("route api/image/[id] error:",error);
         return new NextResponse(null,{status:404});
