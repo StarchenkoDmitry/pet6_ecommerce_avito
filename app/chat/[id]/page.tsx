1061f08a95809802d8dbffe660d9080a211b998f
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-02 03:22:56 +0300

    refactor Chat page

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,48 +1,39 @@
-import { auth } from '@/config/authConfig';
-import db from '@/lib/db';
-import ChatSocket from '@/components/chat/ChatSocket';
-import { findChatByIdAndUserIdWithItem } from '@/lib/db/chat';
+import { auth } from "@/config/authConfig";
+import db from "@/lib/db";
+import { findChatByIdAndUserIdWithItem } from "@/lib/db/chat";
+import ChatSocket from "@/components/chat/ChatSocket";
 
 
 export default async function Home({ params }: { params: { id: string } }) {
-  const { id:chatId } = params;
-  console.log("Render Chat page");
-  
-  const session = await auth();
-  const user = await db.user.currentUser();
-  
-  if(!user || !session){
-    return(
-      <div>
-        Зарегистрируйтесь
-      </div>
+    const { id: chatId } = params;
+    console.log("Render Chat page");
+
+    const session = await auth();
+    const user = await db.user.currentUser();
+
+    if (!user || !session) {
+        return <div>Зарегистрируйтесь</div>;
+    }
+
+    const chat = await findChatByIdAndUserIdWithItem(chatId, user.id);
+
+    if (!chat) {
+        return <div>Chat with id {chatId} is not exist.</div>;
+    }
+
+    const accessToken = session.user.accessToken;
+
+    const userCom = chat.chatUsers.length > 0 ? chat.chatUsers[0].user : undefined;
+
+    return (
+        <div className="flex flex-col flex-1">
+            <ChatSocket
+                token={accessToken}
+                chatId={chatId}
+                me={user}
+                user={userCom}
+                item={chat.item}
+            />
+        </div>
     );
-  }
-
-  const chat = await findChatByIdAndUserIdWithItem(chatId,user.id);
-
-  if(!chat){
-    return(
-      <div>
-        Chat with id {chatId} is not exist.
-      </div>
-    );
-  }
-
-  const accessToken = session.user.accessToken;
-  // console.log("ITME:",chat.chatUsers);
-
-  const userCom = chat.chatUsers.length > 0 ? chat.chatUsers[0].user : undefined;
-
-  return(
-    <div className='flex flex-col flex-1'>
-      <ChatSocket
-        token={accessToken}
-        chatId={chatId}
-        me={user}
-        user={userCom}
-        item={chat.item}
-      />      
-    </div>
-  );
 }
