54df2d5937be37cfb806a4f49feeb93a963e613d
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-02 22:11:11 +0300

    change api avatar

--- app/api/avatar/[id]/route.ts
+++ app/api/avatar/[id]/route.ts
@@ -1,5 +1,9 @@
 import db from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import { 
+    AVATAR_SCALE_WIDTH_0,
+    AVATAR_SCALE_WIDTH_1,
+} from "@/lib/const";
 
 
 export async function GET(
@@ -7,20 +11,53 @@ export async function GET(
     { params }: { params: { id: string } }
 ) {
     try {
+        const searchParams = req.nextUrl.searchParams
+        const widthStr = searchParams.get('w')
         const id = params.id;
 
-        const res = await db.avatarImage.findFirst({
-            where:{
-                id:id
+        console.log("ID: ",id);
+        console.log("width: ",widthStr);
+
+        if(!widthStr){
+            const image = await db.avatarImage.findFirst({
+                where:{ id:id },select:{ buffer:true }
+            });
+            
+            if(image){
+                return new NextResponse(image.buffer);
             }
-        });
-    
-        if(!res){
-            return NextResponse.json({superAuth:1111});
         }else{
-            return new NextResponse(res.buffer);
+            const width = parseInt(widthStr);
+            if(!Number.isFinite(width)){
+                return new NextResponse(null,{status:400});
+            }
+
+            if(width <= AVATAR_SCALE_WIDTH_1){
+                const image = await db.avatarImage.findFirst({
+                    where:{ id:id },select:{ buffer1:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer1);
+                }
+            }else if(width <= AVATAR_SCALE_WIDTH_0){
+                const image = await db.avatarImage.findFirst({
+                    where:{ id:id },select:{ buffer0:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer0);
+                }
+            }else{
+                const image = await db.avatarImage.findFirst({
+                    where:{ id:id },select:{ buffer:true }
+                });
+                if(image){
+                    return new NextResponse(image.buffer);
+                }
+            }
         }
+        return new NextResponse(null,{status:404});
     } catch (error) {
-        return NextResponse.json({},{status:404});
+        console.log("route api/image/[id] error:",error);
+        return new NextResponse(null,{status:404});
     }
 }
