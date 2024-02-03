c93dec2e0126fbf0a9517ef6ad76e18a5eb1488d
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-03 23:39:22 +0300

    change item.service

---
+++ lib/services/item.service.ts
@@ -0,0 +1,107 @@
+import { cookies } from "next/headers";
+import db from "../db";
+import { ItemAndFavorite } from "../types/item";
+import { COOKIE_FAVORITE_KEY, SKIP_ITEMS } from "../const";
+
+
+export async function getItemsForMainPage(
+    
+):Promise<ItemAndFavorite[] | undefined> {
+    try {
+        const user = await db.user.currentUser();
+
+        if (user) {
+            return await getItemsWithFavoriteByUser(user.id,2,1);
+        } else {
+            const tempFavoriteListId = cookies().get(COOKIE_FAVORITE_KEY)?.value;
+            
+            if(tempFavoriteListId){
+                return await getItemsWithFavoriteByTempFavorite(tempFavoriteListId,2,1);
+            } else {
+                return await getItemsWithFavorite(2,2);
+            }
+        }
+    } catch (error) {
+        console.log("getItemsForMainPage error:",error);
+        return;
+    }
+}
+
+
+
+async function getItemsWithFavoriteByUser(
+    userId:string,
+    take:number,
+    skip:number,
+):Promise<ItemAndFavorite[] | undefined> {
+    try {
+        const items = await db.item.findMany({
+            include: {
+                favorites: {
+                    where: { userId },
+                },
+            },
+            skip,
+            take,
+            orderBy: { ceatedAt: "desc" },
+        });
+
+        return items.map(({favorites,...item}) => ({ 
+            ...item, 
+            isFavorite: favorites.length > 0 
+        }));
+    } catch (error) {
+        console.log("getItemsWithFavoriteByUser error:",error);
+        return;
+    }
+}
+
+
+async function getItemsWithFavoriteByTempFavorite(
+    tempFavoriteListId:string,
+    take:number,
+    skip:number,
+):Promise<ItemAndFavorite[] | undefined> {
+    try {
+        const items = await db.item.findMany({
+            include:{
+                tempFavorites:{
+                    where:{ tempFavoriteListId },
+                    take:1
+                }
+            },
+            take,
+            skip,
+            orderBy: { ceatedAt: "desc" },
+        });
+
+        return items.map(({ tempFavorites, ...item }) => ({
+            ...item,
+            isFavorite: tempFavorites.length > 0,
+        }));
+    } catch (error) {
+        console.log("getItemsWithFavoriteByTempFavorite error:",error);
+        return;
+    }
+}
+
+async function getItemsWithFavorite(
+    take:number,
+    skip:number,
+):Promise<ItemAndFavorite[] | undefined> {
+    try {
+        const items = await db.item.findMany({
+            take,
+            skip,
+            orderBy: { ceatedAt: "desc" },
+        });
+
+        return items.map((item) => ({ 
+            ...item, 
+            isFavorite: false 
+        }));
+    } catch (error) {
+        console.log("getItemsWithFavorite error:",error);
+        return;
+    }
+}
