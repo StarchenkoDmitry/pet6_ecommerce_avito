85b05dedae0b3d2491dddcc15c6a3ea3d1625e30
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-04 04:10:24 +0300

    change main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,30 @@
-import Navbar from "../components/ui/Navbar";
 import ItemsSearch from "@/components/item/ItemsSearch";
+import { getCountItems } from "@/lib/actions/item";
 import { getItemsForMainPage } from "@/lib/services/item.service";
 
 
-export default async function Home() {    
-    let items = await getItemsForMainPage();
+export default async function Home() {
+    // const items = await getItemsForMainPage();
+    // const countItems = await getCountItems();
+
+    const [items,countItems] = await Promise.all([
+        getItemsForMainPage(4),
+        getCountItems()
+    ]);
 
     return (
-        <div className="p-2">
-            <Navbar />
-            <h2 className="m-2 my-1 text-xl font-bold">Рекомендации для вас</h2>
+        <div className="flex flex-col flex-1 px-2">
             {
-                items ? 
-                <ItemsSearch items={items}/> : 
-                <div>
-                    error loading
-                </div>
+                items ? (
+                    <ItemsSearch
+                        items={items}
+                        countItems={countItems}
+                    />
+                ):(
+                    <div>
+                        error loading
+                    </div>
+                )
             }
         </div>
     );
