c2bdfa31f80b5ad5bbf00fec6f7c8a9557ac6a71
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-08 11:54:22 +0300

    change auth actions file

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,22 +1,88 @@
 'use server'
+import db from "@/db";
 import { 
     signIn, 
     signOut, 
     update 
 } from "@/config/authConfig";
-import db from "@/db";
+
+import { 
+    MAX_EMAIL_LENGHT, 
+    MAX_NAME_LENGHT, 
+    MAX_PASSWORD_LENGHT, 
+    MAX_SURNAME_LENGHT, 
+    MIN_EMAIL_LENGHT, 
+    MIN_NAME_LENGHT, 
+    MIN_PASSWORD_LENGHT, 
+    MIN_SURNAME_LENGHT
+} from "@/constants";
+
+import {validateEmail} from "@/utils/Validate";
 
 
-export async function signUpWithCredentials(formData: FormData, doSingIn:boolean = true){
+export type SignUpResponse = {
+    signUp:boolean;
+    signIn?:boolean;
+    error?:string;
+    userId?:string;
+}
+
+export async function signUpWithCredentials(
+    formData: FormData, 
+    doSingIn:boolean = true
+):Promise<SignUpResponse>{
     console.log("registerWithCredentials formData: ",formData);
 
     const email = formData.get('email');
     const password = formData.get('password');
 
-    if(typeof email !== "string" || typeof password !== "string"){
+    const name = formData.get('name');
+    const surname = formData.get('surname');
+
+    console.log("SIGN",email,password,name,surname);
+
+    if( typeof email !== "string" || 
+        typeof password !== "string" ||
+        typeof name !== "string" ||
+        typeof surname !== "string"){
         return {
             signUp:false,
-            error:"email or password is not string",
+            error:"email, password, name or surname is not string",
+        };
+    }
+
+    if(email.length > MAX_EMAIL_LENGHT || email.length < MIN_EMAIL_LENGHT ){
+        return {
+            signUp:false,
+            error:`email length is greater than ${MAX_EMAIL_LENGHT} or less than ${MIN_EMAIL_LENGHT}`
+        };
+    }
+
+    if(!validateEmail(email)){
+        return {
+            signUp:false,
+            error:`email filed is not email`
+        };
+    }
+
+    if(password.length > MAX_PASSWORD_LENGHT || password.length < MIN_PASSWORD_LENGHT ){
+        return {
+            signUp:false,
+            error:`password length is greater than ${MAX_PASSWORD_LENGHT} or less than ${MIN_PASSWORD_LENGHT}`
+        };
+    }
+
+    if(name.length > MAX_NAME_LENGHT || name.length < MIN_NAME_LENGHT ){
+        return {
+            signUp:false,
+            error:`name length is greater than ${MAX_NAME_LENGHT} or less than ${MIN_NAME_LENGHT}`
+        };
+    }
+
+    if(surname.length > MAX_SURNAME_LENGHT || surname.length < MIN_SURNAME_LENGHT ){
+        return {
+            signUp:false,
+            error:`surname length is greater than ${MAX_SURNAME_LENGHT} or less than ${MIN_SURNAME_LENGHT}`
         };
     }
 
@@ -34,7 +100,8 @@ export async function signUpWithCredentials(formData: FormData, doSingIn:boolean
 
             const userCreated = await ts.user.create({
                 data:{
-                    name:"DIMKA",
+                    name:name,
+                    surname:surname,
                 }
             });
             
