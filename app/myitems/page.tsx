6cc384553ede6154b285ed381ae7b55f72dfffa3
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-09 16:31:24 +0300

    refactor myitems page

--- app/myitems/page.tsx
+++ app/myitems/page.tsx
@@ -1,26 +1,22 @@
 import db from "@/db";
 import MyItem from "@/components/item/MyItem";
+import { getItemsByUserId } from "@/services/item.service";
+import { MYITEMS_PAGE__TAKE_ITEMS } from "@/constants";
 
 
-const MAX_TAKE_ITEM = 64;
 
 export default async function Home() {
     const user = await db.user.currentUser();
 
     if (user) {
-        const items = await db.item.findMany({
-            where: {
-                userId: user.id,
-            },
-            take:MAX_TAKE_ITEM
-        });
+        const items = await getItemsByUserId(user.id,MYITEMS_PAGE__TAKE_ITEMS)
 
         return (
             <div className="p-2 bg-gray-50 rounded-lg">
                 <span className="mx-1 text-xl">My items</span>
                 <div className="flex flex-wrap justify-between bg-gray-100 rounded-lg">
                     {
-                        items.map((i) => (<MyItem key={i.id} item={i} />))
+                        items?.map((i) => (<MyItem key={i.id} item={i} />))
                     }
                 </div>
             </div>
