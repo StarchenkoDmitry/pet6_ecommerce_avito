7e5e380974f99bfc5d66750b5d618869922e87fc
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-09 16:34:56 +0300

    refactor item page

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -6,73 +6,72 @@ export default async function Home({ params }: { params: { id: string } }) {
     const { id } = params;
 
     const user = await db.user.currentUser();
-
-    if (user) {
-        const item = await db.item.findFirst({
-            where: { id: id },
-            include: {
-                user: {
-                    select: {
-                        id: true,
-                        imageId: true,
-                        name: true,
-                        surname: true,
+    try {
+        if (user) {
+            const item = await db.item.findFirst({
+                where: { id: id },
+                include: {
+                    user: {
+                        select: {
+                            id: true,
+                            imageId: true,
+                            name: true,
+                            surname: true,
+                        },
+                    },
+                    favorites: {
+                        where: {
+                            userId: user.id,
+                        },
                     },
                 },
-                favorites: {
-                    where: {
-                        userId: user.id,
+            });
+
+            if (item) {
+                const owner = item.user ? item.user.id === user.id : false;
+                const favorite = item.favorites.length >= 1;
+
+                return (
+                    <Item 
+                        item={item} 
+                        favorite={favorite} 
+                        isOwner={owner} 
+                        userOwner={item.user} 
+                    />
+                );
+            }
+        } else {
+            const item = await db.item.findFirst({
+                where: { id: id },
+                include: {
+                    user: {
+                        select: {
+                            id: true,
+                            imageId: true,
+                            name: true,
+                            surname: true,
+                        },
                     },
                 },
-            },
-        });
-
-        if (item) {
-            const owner = item.user ? item.user.id === user.id : false;
-            const favorite = item.favorites.length >= 1;
-
-            return (
-                <Item 
-                    item={item} 
-                    favorite={favorite} 
-                    isOwner={owner} 
-                    userOwner={item.user} 
-                />
-            );
+            });
+            if (item) {
+                return (
+                    <Item 
+                        item={item} 
+                        favorite={false} 
+                        isOwner={false} 
+                        userOwner={item.user} 
+                    />
+                );
+            }
         }
-    } else {
-        const item = await db.item.findFirst({
-            where: { id: id },
-            include: {
-                user: {
-                    select: {
-                        id: true,
-                        imageId: true,
-                        name: true,
-                        surname: true,
-                    },
-                },
-            },
-        });
-        if (item) {
-            return (
-                <Item 
-                    item={item} 
-                    favorite={false} 
-                    isOwner={false} 
-                    userOwner={item.user} 
-                />
-            );
-        }
+    } catch (error) {
+        console.log("Item page error:",error);
     }
 
-    return <ItemNotExist id={id} />;
-}
-
-function ItemNotExist({ id }: { id: string }) {
     return (
         <div className="m-2 p-2">
             <h2 className="p-1">Item with id({id}) is not exist</h2>
         </div>
-    );
+    )
 }
