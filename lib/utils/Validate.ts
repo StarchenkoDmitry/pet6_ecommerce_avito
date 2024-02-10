b598a72ca270f941e9308ef63b70e7cd1bae24b2
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-10 10:22:35 +0300

    change Validate file

--- lib/utils/Validate.ts
+++ lib/utils/Validate.ts
@@ -1,8 +1,6 @@
-// source code from https://melvingeorge.me/blog/check-if-string-is-valid-email-address-javascript
-
-// Regular expression to check if string is email
-const regexExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
-
-export function validateEmail(email:string):boolean{
-    return regexExp.test(email);
+export function validateEmail(email: string): boolean {
+    // Regular expression for email verification
+    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    return emailRegex.test(email);
 }
